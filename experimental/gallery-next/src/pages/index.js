c91d28499d4364ab17a67329dc1568cd27e93601
Thorsten Kober <tkober@gmail.com>
2024-10-22 16:45:56 -0400

    initial commit

---
+++ experimental/gallery-next/src/pages/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+import data from "public/data/unsplash.json";
+import Page from "@/partials/page/page"
+import JustifiedLayout from "@/components/justified-layout/justified-layout";
+
+export default function Home() {
+    return (
+        <>
+          <Head>
+                <title>Photos Justified!</title>
+                <meta name="description" content="A justified layout for photos with Next.js." key="description" />
+                <meta httpEquiv="Permissions-Policy" content="interest-cohort=()" />
+            </Head>
+            <Page id="index">
+              <JustifiedLayout data={data} />
+            </Page>
+        </>
+    );
+}
