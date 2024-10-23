0b62bca7c6dc29410412f4301fe5ca076d28ac75
Thorsten Kober <tkober@gmail.com>
2024-10-23 17:04:57 -0400

    check-in"

--- experimental/gallery-next/src/partials/layout/layout.jsx
+++ experimental/gallery-next/src/partials/layout/layout.jsx
@@ -3,11 +3,13 @@ import Main from "@/partials/main/main";
 import Footer from "@/partials/footer/footer";
 
 import styles from "./layout.module.css";
+import Navigation from "@/partials/navigation/navigation";
 
 export default function Layout({ children }) {
     return (
         <div className={styles.layout}>
             <Header />
+            <Navigation />
             <Main>{children}</Main>
             <Footer />
         </div>
